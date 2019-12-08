438aa0947fd1ee21a617e4ef9d389a3bb0dee4e4
Mathieu Benoit <mabenoit@microsoft.com>
2019-12-07 22:23:50 -0500

    netcoreapp3.1

--- parrot/Startup.cs
+++ parrot/Startup.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
 namespace parrot
 {
@@ -22,17 +18,14 @@ namespace parrot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<DaemonHub>(new DaemonHub());
-            services.AddMvc();
+            services.AddControllersWithViews();
+            services.AddSingleton(new DaemonHub());
             services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,16 +36,11 @@ namespace parrot
             }
 
             app.UseStaticFiles();
-
-            app.UseSignalR(routes => {
-                routes.MapHub<DaemonHub>("/daemonhub");
-            });
-
-            app.UseMvc(routes =>
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<DaemonHub>("/daemonhub");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
